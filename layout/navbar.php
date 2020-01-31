87bac3b18e36909592258d5de09ed12c52ed0448
Tuomas <tuomas.puro@esedu.fi>
2020-01-31 14:49:43 +0200

    Layout changes

--- layout/navbar.php
+++ layout/navbar.php
@@ -2,24 +2,36 @@
 
   <!-- Links -->
   <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="register.php">Register</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="show_users.php">Show users</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="login.php">Login</a>
-    </li>
-    <li>
-        <form class="form-inline" action="/action_page.php">
-            <label for="email" class="mr-sm-2">Email address:</label>
-            <input type="email" class="form-control mb-2 mr-sm-2" id="email">
-            <label for="pwd" class="mr-sm-2">Password:</label>
-            <input type="password" class="form-control mb-2 mr-sm-2" id="pwd">
-            <button type="submit" class="btn btn-primary mb-2">Submit</button>
-        </form>
-    </li>
+    
+    
+  <?php if (isset($_SESSION['user_id'])): ?> 
+      
+      <!-- Logged in -menu -->
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="show_users.php">Show users</a>
+      </li>
+
+      <li class="nav-item">
+        <a href="show_topics.php" class="nav-link">Show topics</a>      
+      </li>
+
+  <?php else: ?>
+
+      <!-- Logged out -menu -->
+      <li>
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="register.php">Register</a>
+      </li>
+      
+  <?php endif; ?>
+
+    
   </ul>
 
 </nav>
