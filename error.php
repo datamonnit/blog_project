559510758212c63f95239bd645989740127f6b56
Tuomas <tuomas.puro@esedu.fi>
2020-01-31 14:50:42 +0200

    new error system

--- error.php
+++ error.php
@@ -1,9 +1,14 @@
 <?php
-if (!isset($_GET['msg'])) {
-    $msg = 'General error! Sorry!';
-} else {
+session_start();
+
+if(isset($_SESSION['error'])) {
+    $msg = $_SESSION['error'];
+} else if (isset($_GET['msg'])) {
     $msg = urldecode($_GET['msg']);
+} else {
+    $msg = 'General error! Sorry!';
 }
+
 ?>
 <h1>Error</h1>
 <p><?php echo $msg; ?></p>
